c08c2efa716e2dee589eebd3dbf07c749bd0a0ce
Nikhil S Bharadwaj <nikhils24101998@gmail.com>
2024-01-14 14:02:56 +0530

    Refactor NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,56 +1,98 @@
 "use client";
 import Link from "next/link";
-import React from "react";
-import classnames from "classnames";
 import { usePathname } from "next/navigation";
+import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
+import {
+    Avatar,
+    Box,
+    Container,
+    DropdownMenu,
+    Flex,
+    Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
+    return (
+        <nav className="border-b mb-5 px-5 py-3">
+            <Container>
+                <Flex justify="between">
+                    <Flex align="center" gap="3">
+                        <Link href="/">
+                            <AiFillBug />
+                        </Link>
+                        <NavLinks />
+                    </Flex>
+                    <AuthStatus />
+                </Flex>
+            </Container>
+        </nav>
+    );
+};
+
+const NavLinks = () => {
     const currentPath = usePathname();
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Issues", href: "/issues/list" },
+    ];
+
+    return (
+        <ul className="flex space-x-6">
+            {links.map((link) => (
+                <li key={link.href}>
+                    <Link
+                        className={classnames({
+                            "nav-link": true,
+                            "!text-zinc-900": link.href === currentPath,
+                        })}
+                        href={link.href}
+                    >
+                        {link.label}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const AuthStatus = () => {
     const { status, data: session } = useSession();
 
-    const links = [
-        {
-            label: "Dashboard",
-            href: "/",
-        },
-        {
-            label: "Issues",
-            href: "/issues",
-        },
-    ];
-    return (
-        <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-            <Link href="/">
-                <AiFillBug />
+    if (status === "loading") return <Skeleton width="3rem" />;
+    if (status === "unauthenticated")
+        return (
+            <Link href="/api/auth/signin" className="nav-link">
+                Login
             </Link>
-            <ul className="flex space-x-6">
-                {links.map((link) => (
-                    <li key={link.href}>
-                        <Link
-                            className={classnames({
-                                "text-zinc-950": link.href == currentPath,
-                                "text-zinc-500": link.href != currentPath,
-                                "hover:text-zinc-800 transition-colors": true,
-                            })}
-                            href={link.href}
-                        >
-                            {link.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <Box>
-                {status === "authenticated" && (
-                    <Link href="/api/auth/signout">Log out</Link>
-                )}
-                {status === "unauthenticated" && (
-                    <Link href="/api/auth/signin">Login</Link>
-                )}
-            </Box>
-        </nav>
+        );
+
+    return (
+        <Box>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <Avatar
+                        src={session!.user!.image!}
+                        fallback="?"
+                        size="2"
+                        radius="full"
+                        className="cursor-pointer"
+                        referrerPolicy="no-referrer"
+                    />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                        <Text size="2">{session!.user!.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                        <Link href="/api/auth/signout">Log out</Link>
+                    </DropdownMenu.Item>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </Box>
     );
 };
 
