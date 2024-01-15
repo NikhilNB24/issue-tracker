258461f56a99fbf6c9ba11e1db2e7e50dcfb4aaf
Nikhil S Bharadwaj <nikhils24101998@gmail.com>
2024-01-15 14:59:22 +0530

    Issue Count on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+import { prisma } from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
 import Pagination from "./components/Pagination";
 
-export default function Home({
-    searchParams,
-}: {
-    searchParams: { page: string };
-}) {
+export default async function Home() {
+    const openIssueCount = await prisma.issue.count({
+        where: {
+            status: "OPEN",
+        },
+    });
+    const inProgressIssueCount = await prisma.issue.count({
+        where: {
+            status: "IN_PROGRESS",
+        },
+    });
+    const closedIssueCount = await prisma.issue.count({
+        where: {
+            status: "CLOSED",
+        },
+    });
     return (
-        <Pagination
-            itemCount={100}
-            pageSize={10}
-            currentPage={parseInt(searchParams.page)}
+        <IssueSummary
+            open={openIssueCount}
+            inProgress={inProgressIssueCount}
+            closed={closedIssueCount}
         />
     );
 }
